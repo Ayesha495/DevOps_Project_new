a88afb38fddd3e2f32dd415628b9f0430cca94f5
Jannat Fatima <jatuu786@gmail.com>
2026-04-20 21:27:19 +0500

    tested pipeline by jannat

--- calculator_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = {
-    path('', views.home, name='home'),
-    path('/bug', views.bug, name="bug"),
-    path('/enhancement', views.enhancement, name="enhancement")
-}
